96d35c5364552dc4d5490e48328a2dc7ae7e8a66
Heran <choudouhu@163.com>
2015-09-23 16:13:18 +0800

    smtp config

---
+++ smtp.config.php
@@ -0,0 +1,6 @@
+<?php
+$smtpconfig = new stdClass();
+$smtpconfig->server = "";//SMTP服务器
+$smtpconfig->port = 25;//SMTP服务器端口
+$smtpconfig->from = $smtpconfig->user = "";//SMTP服务器的用户邮箱
+$smtpconfig->pass = "";//发送给谁
